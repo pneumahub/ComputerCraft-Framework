ae64a09d611015d7b1cb1caff025ae87b55c4d6a
pneumahub <39502544+pneumahub@users.noreply.github.com>
2022-01-11 17:26:57 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local framework = {};
 
 framework.require = function(path)
-    local r = loadstring(http.get("https://raw.githubusercontent.com/pneumahub/ComputerCraft-Framework/"..path));
+    local r = loadstring(http.get("https://raw.githubusercontent.com/pneumahub/ComputerCraft-Framework/"..path).readAll());
     return r and r() or error("Failed to load "..path);
 end
 
