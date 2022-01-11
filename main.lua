fb049db49be5291dcb09c0e5f7608f0a7b37cee7
pneumahub <39502544+pneumahub@users.noreply.github.com>
2022-01-11 17:21:55 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,7 @@
-print("YES");
+local framework = {};
+
+framework.require = function(path)
+    return loadstring(http.get("https://raw.githubusercontent.com/pneumahub/ComputerCraft-Framework/"..path))();
+end
+
+return framework;
