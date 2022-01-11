2bc719b643a98bf14fddca64b08031f5efa6ae85
pneumahub <39502544+pneumahub@users.noreply.github.com>
2022-01-11 17:36:57 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
 local framework = {};
 
 framework.require = function(path)
-    local r = loadstring(http.get("https://raw.githubusercontent.com/pneumahub/ComputerCraft-Framework/"..path).readAll());
-    return r and r() or error("Failed to load "..path);
+    local h = http.get("https://raw.githubusercontent.com/pneumahub/ComputerCraft-Framework/"..path);
+    if h==nil then return error("Failed to load "..path..": DOES NOT EXIST", 2) end
+    local r = loadstring();
+    return r and r() or error("Failed to load "..path..": DID NOT RETURN VALUE", 2);
 end
 
 return framework;
